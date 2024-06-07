304013db716ec7ee7db4c87e95f1922e34389724
Shubham Bhandari <dev.sb18@gmail.com>
2024-06-07 17:29:15 +0200

    updated application with models, introduction and model description

--- application/app.py
+++ application/app.py
@@ -1,12 +1,40 @@
 import streamlit as st
-from loader import load_model, preprocess_image, display_grad_cam,load_model_image, get_grad_cam
+from loader import load_model, preprocess_image, display_grad_cam,load_model_image, get_grad_cam, get_model_description, plot_colormap_legend
+from button import create_button
 from PIL import Image
 
-add_selectbox = st.sidebar.selectbox(
-    'Select a Model',
-    ('resnet_152')
+import streamlit as st
+
+st.markdown(create_button(), unsafe_allow_html=True)
+
+st.title('Object Detection Explained')
+with st.expander("About the Application"):
+    st.write("""Welcome to our interactive application that helps you understand how AI models detect objects in images!
+
+Ever wondered how a computer knows where the cat or dog is in a photo? AI models used for image recognition often seem like magic because it's hard to see how they make their decisions. But with a technique called Grad-CAM, we can peek inside these "black boxes" and see what’s going on.
+
+In this app, you can upload an image and discover which areas influenced the model's decision the most. This way, you can see exactly why the model thinks there’s a cat, dog, or other objects in the picture. By visualizing this process, Grad-CAM helps us build trust and understand how artificial intelligence works.
+
+[Learn more about Grad-CAM](https://pub.towardsai.net/explaining-deep-neural-networks-gradcam-e678a848ad44)
+    """)
+
+with st.expander("Instructions"):
+    st.write("""
+        1. Select the model that you want to test from the sidebar.
+        2. Upload an image to perform object detection and model explanation.
+        3. Once the image has been processed you can see what features the neural network considered to detect the object in the image.
+        4. Review the feature usage heatmap for better understanding of the colormap distribution.
+    """)
+about_model=get_model_description()
+st.sidebar.subheader('Select a Model')
+add_selectbox = st.sidebar.selectbox('Models available',
+    list(about_model.keys())
 )
-model, classes = load_model(add_selectbox)
+st.sidebar.divider()
+st.sidebar.subheader('Model Description')
+model, classes, layer_name = load_model(about_model[add_selectbox]['url'], about_model[add_selectbox]['layer_name'])
+st.sidebar.markdown(about_model[add_selectbox]['description'])
+
 
 original_image=st.file_uploader(label='Upload an image')
 
@@ -15,17 +43,22 @@ original, processed=st.columns(spec=[0.5,0.5])
 if original_image is not None:
     original_image = Image.open(original_image)
     with st.spinner("Processing..."):
-        st.toast('Image uploaded successfully')
         with original:
-            st.header('Image')
+            st.subheader('Image')
             st.image(original_image, use_column_width=True)
             classes_score=load_model_image(model,original_image, classes)
             processed_image=preprocess_image(original_image)
             display_image = preprocess_image(original_image, display=True)
-            layer_name='layer4'
             grad_cams, values, indices=get_grad_cam(model, processed_image, layer_name=layer_name)
         with processed:
-                st.header('Result')
+                st.subheader('Result')
                 heatmap_img=display_grad_cam(grad_cams, display_image)
                 st.image(heatmap_img)
                 st.text(f'Identification: {classes_score[0][1]}')
+                
+st.sidebar.divider()
+st.sidebar.subheader("Feature usage")
+st.sidebar.pyplot(plot_colormap_legend())
+st.sidebar.markdown("<div style='display: flex; justify-content: space-between;'><span>Low</span><span>High</span></div>", unsafe_allow_html=True)
+
+
--- application/loader.py
+++ application/loader.py
@@ -1,4 +1,4 @@
-from torchvision import transforms
+from torchvision import transforms, models
 import torch
 import pickle
 import urllib.request
@@ -7,14 +7,19 @@ import cv2
 from PIL import Image
 from gradcam_algo import GradCAM
 import torch
+import json
+import matplotlib.pyplot as plt
 
-def load_model(model_name):
-    if model_name=='resnet_152':
-        model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
-        model.eval()
-        
-        classes = pickle.load(urllib.request.urlopen('https://gist.githubusercontent.com/yrevar/6135f1bd8dcf2e0cc683/raw/d133d61a09d7e5a3b36b8c111a8dd5c4b5d560ee/imagenet1000_clsid_to_human.pkl'))
-    return model, classes
+def get_model_description():
+    with open('application/model_description.json','r') as f:
+        about_model=json.load(f)
+    return about_model
+
+def load_model(url, layer_name):
+    model = torch.hub.load('pytorch/vision:v0.10.0', url, pretrained=True)
+    model.eval()
+    classes = pickle.load(urllib.request.urlopen('https://gist.githubusercontent.com/yrevar/6135f1bd8dcf2e0cc683/raw/d133d61a09d7e5a3b36b8c111a8dd5c4b5d560ee/imagenet1000_clsid_to_human.pkl'))
+    return model, classes, layer_name
 
 def preprocess_image(image, display=False):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -44,7 +49,7 @@ def display_grad_cam(cams, display_image):
         cam_numpy = cv2.resize(cam_numpy, (image_np.shape[1], image_np.shape[0]))
         cam_numpy = np.uint8(255 * cam_numpy)
         cam_numpy = cv2.applyColorMap(cam_numpy, cv2.COLORMAP_TURBO)
-        heatmap = cv2.addWeighted(image_np, 0.2, cam_numpy, 0.4, 0)
+        heatmap = cv2.addWeighted(image_np, 0.6, cam_numpy, 0.4, 0)
     heatmap_pil = Image.fromarray(heatmap)
     return heatmap_pil
     
@@ -71,3 +76,19 @@ def get_grad_cam(model, image, layer_name):
         grad_cams.append(cam)
         
     return grad_cams, values, indices
+
+def generate_colormap_legend():
+    colormap = cv2.applyColorMap(np.linspace(0, 255, 256).astype(np.uint8), cv2.COLORMAP_TURBO)
+    colormap = colormap.reshape((1, 256, 3))
+    return colormap
+
+def plot_colormap_legend():
+    
+    colormap = generate_colormap_legend()
+    fig, ax = plt.subplots(figsize=(15, 2))
+    ax.imshow(colormap, aspect='auto')
+    ax.set_xticks([0, 64, 128, 192, 255])
+    ax.set_xticklabels([])
+    ax.set_yticks([])
+
+    return fig
